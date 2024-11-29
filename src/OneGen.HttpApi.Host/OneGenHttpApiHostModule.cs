2c052590ee5a6eeff15be33faf96e9eda71f71e6
Ngoc Ngo <ngocnv371@gmail.com>
2024-11-29 06:17:33 -0800

    add hangfire

--- src/OneGen.HttpApi.Host/OneGenHttpApiHostModule.cs
+++ src/OneGen.HttpApi.Host/OneGenHttpApiHostModule.cs
@@ -39,228 +39,240 @@ using Volo.Abp.OpenIddict;
 using Volo.Abp.Swashbuckle;
 using Volo.Abp.Studio.Client.AspNetCore;
 using Volo.Abp.Security.Claims;
+using Volo.Abp.BackgroundJobs.Hangfire;
+using Hangfire;
 
 namespace OneGen;
 
 [DependsOn(
-    typeof(OneGenHttpApiModule),
-    typeof(AbpStudioClientAspNetCoreModule),
-    typeof(AbpAspNetCoreMvcUiLeptonXLiteThemeModule),
-    typeof(AbpAutofacModule),
-    typeof(AbpAspNetCoreMultiTenancyModule),
-    typeof(OneGenApplicationModule),
-    typeof(OneGenEntityFrameworkCoreModule),
-    typeof(AbpAccountWebOpenIddictModule),
-    typeof(AbpSwashbuckleModule),
-    typeof(AbpAspNetCoreSerilogModule)
-    )]
+	typeof(OneGenHttpApiModule),
+	typeof(AbpStudioClientAspNetCoreModule),
+	typeof(AbpAspNetCoreMvcUiLeptonXLiteThemeModule),
+	typeof(AbpAutofacModule),
+	typeof(AbpAspNetCoreMultiTenancyModule),
+	typeof(OneGenApplicationModule),
+	typeof(OneGenEntityFrameworkCoreModule),
+	typeof(AbpAccountWebOpenIddictModule),
+	typeof(AbpSwashbuckleModule),
+	typeof(AbpAspNetCoreSerilogModule),
+	typeof(AbpBackgroundJobsHangfireModule)
+	)]
 public class OneGenHttpApiHostModule : AbpModule
 {
-    public override void PreConfigureServices(ServiceConfigurationContext context)
-    {
-        var hostingEnvironment = context.Services.GetHostingEnvironment();
-        var configuration = context.Services.GetConfiguration();
+	public override void PreConfigureServices(ServiceConfigurationContext context)
+	{
+		var hostingEnvironment = context.Services.GetHostingEnvironment();
+		var configuration = context.Services.GetConfiguration();
 
-        PreConfigure<OpenIddictBuilder>(builder =>
-        {
-            builder.AddValidation(options =>
-            {
-                options.AddAudiences("OneGen");
-                options.UseLocalServer();
-                options.UseAspNetCore();
-            });
-        });
+		PreConfigure<OpenIddictBuilder>(builder =>
+		{
+			builder.AddValidation(options =>
+			{
+				options.AddAudiences("OneGen");
+				options.UseLocalServer();
+				options.UseAspNetCore();
+			});
+		});
 
-        if (!hostingEnvironment.IsDevelopment())
-        {
-            PreConfigure<AbpOpenIddictAspNetCoreOptions>(options =>
-            {
-                options.AddDevelopmentEncryptionAndSigningCertificate = false;
-            });
+		if (!hostingEnvironment.IsDevelopment())
+		{
+			PreConfigure<AbpOpenIddictAspNetCoreOptions>(options =>
+			{
+				options.AddDevelopmentEncryptionAndSigningCertificate = false;
+			});
 
-            PreConfigure<OpenIddictServerBuilder>(serverBuilder =>
-            {
-                serverBuilder.AddProductionEncryptionAndSigningCertificate("openiddict.pfx", configuration["AuthServer:CertificatePassPhrase"]!);
-                serverBuilder.SetIssuer(new Uri(configuration["AuthServer:Authority"]!));
-            });
-        }
-    }
+			PreConfigure<OpenIddictServerBuilder>(serverBuilder =>
+			{
+				serverBuilder.AddProductionEncryptionAndSigningCertificate("openiddict.pfx", configuration["AuthServer:CertificatePassPhrase"]!);
+				serverBuilder.SetIssuer(new Uri(configuration["AuthServer:Authority"]!));
+			});
+		}
+	}
 
-    public override void ConfigureServices(ServiceConfigurationContext context)
-    {
-        var configuration = context.Services.GetConfiguration();
-        var hostingEnvironment = context.Services.GetHostingEnvironment();
+	public override void ConfigureServices(ServiceConfigurationContext context)
+	{
+		var configuration = context.Services.GetConfiguration();
+		var hostingEnvironment = context.Services.GetHostingEnvironment();
 
-        if (!configuration.GetValue<bool>("App:DisablePII"))
-        {
-            Microsoft.IdentityModel.Logging.IdentityModelEventSource.ShowPII = true;
-            Microsoft.IdentityModel.Logging.IdentityModelEventSource.LogCompleteSecurityArtifact = true;
-        }
+		if (!configuration.GetValue<bool>("App:DisablePII"))
+		{
+			Microsoft.IdentityModel.Logging.IdentityModelEventSource.ShowPII = true;
+			Microsoft.IdentityModel.Logging.IdentityModelEventSource.LogCompleteSecurityArtifact = true;
+		}
 
-        if (!configuration.GetValue<bool>("AuthServer:RequireHttpsMetadata"))
-        {
-            Configure<OpenIddictServerAspNetCoreOptions>(options =>
-            {
-                options.DisableTransportSecurityRequirement = true;
-            });
-            
-            Configure<ForwardedHeadersOptions>(options =>
-            {
-                options.ForwardedHeaders = ForwardedHeaders.XForwardedProto;
-            });
-        }
+		if (!configuration.GetValue<bool>("AuthServer:RequireHttpsMetadata"))
+		{
+			Configure<OpenIddictServerAspNetCoreOptions>(options =>
+			{
+				options.DisableTransportSecurityRequirement = true;
+			});
 
-        ConfigureAuthentication(context);
-        ConfigureUrls(configuration);
-        ConfigureBundles();
-        ConfigureConventionalControllers();
-        ConfigureSwagger(context, configuration);
-        ConfigureVirtualFileSystem(context);
-        ConfigureCors(context, configuration);
-    }
+			Configure<ForwardedHeadersOptions>(options =>
+			{
+				options.ForwardedHeaders = ForwardedHeaders.XForwardedProto;
+			});
+		}
 
-    private void ConfigureAuthentication(ServiceConfigurationContext context)
-    {
-        context.Services.ForwardIdentityAuthenticationForBearer(OpenIddictValidationAspNetCoreDefaults.AuthenticationScheme);
-        context.Services.Configure<AbpClaimsPrincipalFactoryOptions>(options =>
-        {
-            options.IsDynamicClaimsEnabled = true;
-        });
-    }
+		ConfigureAuthentication(context);
+		ConfigureUrls(configuration);
+		ConfigureBundles();
+		ConfigureConventionalControllers();
+		ConfigureSwagger(context, configuration);
+		ConfigureVirtualFileSystem(context);
+		ConfigureCors(context, configuration);
+		ConfigureHangfire(context, configuration);
+	}
 
-    private void ConfigureUrls(IConfiguration configuration)
-    {
-        Configure<AppUrlOptions>(options =>
-        {
-            options.Applications["MVC"].RootUrl = configuration["App:SelfUrl"];
-            options.Applications["Angular"].RootUrl = configuration["App:AngularUrl"];
-            options.Applications["Angular"].Urls[AccountUrlNames.PasswordReset] = "account/reset-password";
-            options.RedirectAllowedUrls.AddRange(configuration["App:RedirectAllowedUrls"]?.Split(',') ?? Array.Empty<string>());
-        });
-    }
+	private void ConfigureHangfire(ServiceConfigurationContext context, IConfiguration configuration)
+	{
+		context.Services.AddHangfire(config =>
+		{
+			config.UseSqlServerStorage(configuration.GetConnectionString("Default"));
+		});
+	}
 
-    private void ConfigureBundles()
-    {
-        Configure<AbpBundlingOptions>(options =>
-        {
-            options.StyleBundles.Configure(
-                LeptonXLiteThemeBundles.Styles.Global,
-                bundle =>
-                {
-                    bundle.AddFiles("/global-styles.css");
-                }
-            );
-        });
-    }
+	private void ConfigureAuthentication(ServiceConfigurationContext context)
+	{
+		context.Services.ForwardIdentityAuthenticationForBearer(OpenIddictValidationAspNetCoreDefaults.AuthenticationScheme);
+		context.Services.Configure<AbpClaimsPrincipalFactoryOptions>(options =>
+		{
+			options.IsDynamicClaimsEnabled = true;
+		});
+	}
 
+	private void ConfigureUrls(IConfiguration configuration)
+	{
+		Configure<AppUrlOptions>(options =>
+		{
+			options.Applications["MVC"].RootUrl = configuration["App:SelfUrl"];
+			options.Applications["Angular"].RootUrl = configuration["App:AngularUrl"];
+			options.Applications["Angular"].Urls[AccountUrlNames.PasswordReset] = "account/reset-password";
+			options.RedirectAllowedUrls.AddRange(configuration["App:RedirectAllowedUrls"]?.Split(',') ?? Array.Empty<string>());
+		});
+	}
 
-    private void ConfigureVirtualFileSystem(ServiceConfigurationContext context)
-    {
-        var hostingEnvironment = context.Services.GetHostingEnvironment();
+	private void ConfigureBundles()
+	{
+		Configure<AbpBundlingOptions>(options =>
+		{
+			options.StyleBundles.Configure(
+				LeptonXLiteThemeBundles.Styles.Global,
+				bundle =>
+				{
+					bundle.AddFiles("/global-styles.css");
+				}
+			);
+		});
+	}
 
-        if (hostingEnvironment.IsDevelopment())
-        {
-            Configure<AbpVirtualFileSystemOptions>(options =>
-            {
-                options.FileSets.ReplaceEmbeddedByPhysical<OneGenDomainSharedModule>(Path.Combine(hostingEnvironment.ContentRootPath, $"..{Path.DirectorySeparatorChar}OneGen.Domain.Shared"));
-                options.FileSets.ReplaceEmbeddedByPhysical<OneGenDomainModule>(Path.Combine(hostingEnvironment.ContentRootPath, $"..{Path.DirectorySeparatorChar}OneGen.Domain"));
-                options.FileSets.ReplaceEmbeddedByPhysical<OneGenApplicationContractsModule>(Path.Combine(hostingEnvironment.ContentRootPath, $"..{Path.DirectorySeparatorChar}OneGen.Application.Contracts"));
-                options.FileSets.ReplaceEmbeddedByPhysical<OneGenApplicationModule>(Path.Combine(hostingEnvironment.ContentRootPath, $"..{Path.DirectorySeparatorChar}OneGen.Application"));
-            });
-        }
-    }
+	private void ConfigureVirtualFileSystem(ServiceConfigurationContext context)
+	{
+		var hostingEnvironment = context.Services.GetHostingEnvironment();
 
-    private void ConfigureConventionalControllers()
-    {
-        Configure<AbpAspNetCoreMvcOptions>(options =>
-        {
-            options.ConventionalControllers.Create(typeof(OneGenApplicationModule).Assembly);
-        });
-    }
+		if (hostingEnvironment.IsDevelopment())
+		{
+			Configure<AbpVirtualFileSystemOptions>(options =>
+			{
+				options.FileSets.ReplaceEmbeddedByPhysical<OneGenDomainSharedModule>(Path.Combine(hostingEnvironment.ContentRootPath, $"..{Path.DirectorySeparatorChar}OneGen.Domain.Shared"));
+				options.FileSets.ReplaceEmbeddedByPhysical<OneGenDomainModule>(Path.Combine(hostingEnvironment.ContentRootPath, $"..{Path.DirectorySeparatorChar}OneGen.Domain"));
+				options.FileSets.ReplaceEmbeddedByPhysical<OneGenApplicationContractsModule>(Path.Combine(hostingEnvironment.ContentRootPath, $"..{Path.DirectorySeparatorChar}OneGen.Application.Contracts"));
+				options.FileSets.ReplaceEmbeddedByPhysical<OneGenApplicationModule>(Path.Combine(hostingEnvironment.ContentRootPath, $"..{Path.DirectorySeparatorChar}OneGen.Application"));
+			});
+		}
+	}
 
-    private static void ConfigureSwagger(ServiceConfigurationContext context, IConfiguration configuration)
-    {
-        context.Services.AddAbpSwaggerGenWithOidc(
-            configuration["AuthServer:Authority"]!,
-            ["OneGen"],
-            [AbpSwaggerOidcFlows.AuthorizationCode],
-            null,
-            options =>
-            {
-                options.SwaggerDoc("v1", new OpenApiInfo { Title = "OneGen API", Version = "v1" });
-                options.DocInclusionPredicate((docName, description) => true);
-                options.CustomSchemaIds(type => type.FullName);
-            });
-    }
+	private void ConfigureConventionalControllers()
+	{
+		Configure<AbpAspNetCoreMvcOptions>(options =>
+		{
+			options.ConventionalControllers.Create(typeof(OneGenApplicationModule).Assembly);
+		});
+	}
 
-    private void ConfigureCors(ServiceConfigurationContext context, IConfiguration configuration)
-    {
-        context.Services.AddCors(options =>
-        {
-            options.AddDefaultPolicy(builder =>
-            {
-                builder
-                    .WithOrigins(
-                        configuration["App:CorsOrigins"]?
-                            .Split(",", StringSplitOptions.RemoveEmptyEntries)
-                            .Select(o => o.Trim().RemovePostFix("/"))
-                            .ToArray() ?? Array.Empty<string>()
-                    )
-                    .WithAbpExposedHeaders()
-                    .SetIsOriginAllowedToAllowWildcardSubdomains()
-                    .AllowAnyHeader()
-                    .AllowAnyMethod()
-                    .AllowCredentials();
-            });
-        });
-    }
+	private static void ConfigureSwagger(ServiceConfigurationContext context, IConfiguration configuration)
+	{
+		context.Services.AddAbpSwaggerGenWithOidc(
+			configuration["AuthServer:Authority"]!,
+			["OneGen"],
+			[AbpSwaggerOidcFlows.AuthorizationCode],
+			null,
+			options =>
+			{
+				options.SwaggerDoc("v1", new OpenApiInfo { Title = "OneGen API", Version = "v1" });
+				options.DocInclusionPredicate((docName, description) => true);
+				options.CustomSchemaIds(type => type.FullName);
+			});
+	}
 
-    public override void OnApplicationInitialization(ApplicationInitializationContext context)
-    {
-        var app = context.GetApplicationBuilder();
-        var env = context.GetEnvironment();
+	private void ConfigureCors(ServiceConfigurationContext context, IConfiguration configuration)
+	{
+		context.Services.AddCors(options =>
+		{
+			options.AddDefaultPolicy(builder =>
+			{
+				builder
+					.WithOrigins(
+						configuration["App:CorsOrigins"]?
+							.Split(",", StringSplitOptions.RemoveEmptyEntries)
+							.Select(o => o.Trim().RemovePostFix("/"))
+							.ToArray() ?? Array.Empty<string>()
+					)
+					.WithAbpExposedHeaders()
+					.SetIsOriginAllowedToAllowWildcardSubdomains()
+					.AllowAnyHeader()
+					.AllowAnyMethod()
+					.AllowCredentials();
+			});
+		});
+	}
 
-        app.UseForwardedHeaders();
+	public override void OnApplicationInitialization(ApplicationInitializationContext context)
+	{
+		var app = context.GetApplicationBuilder();
+		var env = context.GetEnvironment();
 
-        if (env.IsDevelopment())
-        {
-            app.UseDeveloperExceptionPage();
-        }
+		app.UseForwardedHeaders();
 
-        app.UseAbpRequestLocalization();
+		if (env.IsDevelopment())
+		{
+			app.UseDeveloperExceptionPage();
+		}
 
-        if (!env.IsDevelopment())
-        {
-            app.UseErrorPage();
-        }
-        
-        app.MapAbpStaticAssets();
-        app.UseAbpStudioLink();
-        app.UseRouting();
-        app.UseAbpSecurityHeaders();
-        app.UseCors();
-        app.UseAuthentication();
-        app.UseAbpOpenIddictValidation();
+		app.UseAbpRequestLocalization();
 
-        if (MultiTenancyConsts.IsEnabled)
-        {
-            app.UseMultiTenancy();
-        }
+		if (!env.IsDevelopment())
+		{
+			app.UseErrorPage();
+		}
 
-        app.UseUnitOfWork();
-        app.UseDynamicClaims();
-        app.UseAuthorization();
+		app.MapAbpStaticAssets();
+		app.UseAbpStudioLink();
+		app.UseRouting();
+		app.UseAbpSecurityHeaders();
+		app.UseCors();
+		app.UseAuthentication();
+		app.UseAbpOpenIddictValidation();
 
-        app.UseSwagger();
-        app.UseAbpSwaggerUI(options =>
-        {
-            options.SwaggerEndpoint("/swagger/v1/swagger.json", "OneGen API");
+		if (MultiTenancyConsts.IsEnabled)
+		{
+			app.UseMultiTenancy();
+		}
 
-            var configuration = context.ServiceProvider.GetRequiredService<IConfiguration>();
-            options.OAuthClientId(configuration["AuthServer:SwaggerClientId"]);
-        });
-        app.UseAuditing();
-        app.UseAbpSerilogEnrichers();
-        app.UseConfiguredEndpoints();
-    }
+		app.UseUnitOfWork();
+		app.UseDynamicClaims();
+		app.UseAuthorization();
+
+		app.UseSwagger();
+		app.UseAbpSwaggerUI(options =>
+		{
+			options.SwaggerEndpoint("/swagger/v1/swagger.json", "OneGen API");
+
+			var configuration = context.ServiceProvider.GetRequiredService<IConfiguration>();
+			options.OAuthClientId(configuration["AuthServer:SwaggerClientId"]);
+		});
+		app.UseAuditing();
+		app.UseAbpSerilogEnrichers();
+		app.UseHangfireDashboard();
+		app.UseConfiguredEndpoints();
+	}
 }
